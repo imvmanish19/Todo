c90b927bf69e17f27c9a1ac9505e5fae5142ef8b
Manish V <imvmanish19@gmail.com>
2020-05-31 17:44:39 +0530

    Responsiveness added

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ let add = $('#btnAdd')
 let clear = $('#btnClear')
 let task = $('#inpText')
 
-add.click(() => {
-    let element = $('<li>', {
-        'class': 'list-group-item',
-        'text': task.val()
-    })
-    element.click(() => {
-        element.toggleClass('done')
-    })
-    ul.append(element)
+function additem() {
+    if(task.val()){
+        let element = $('<li>', {
+            'class': 'list-group-item',
+            'text': task.val()
+        })
+        element.click(() => {
+            element.toggleClass('done')
+        })
+        ul.append(element)
+    }
+}
+
+add.click(additem);
+
+task.keypress((e) => {
+    if(e.which == 13) 
+    {
+        additem();
+    }
 })
 
 clear.click(() => task.val(''))
